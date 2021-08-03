d3df1fe62fee19dd371ca15c485d9d9bdd9bd135
Magnus Balzer <35585143+UnlimitedHummus@users.noreply.github.com>
2021-08-03 20:47:55 +0200

    made animate.py work without external DSM source

--- animate.py
+++ animate.py
@@ -4,19 +4,26 @@ from fast_shadow import shadow_calc
 import imageio
 import numpy as np
 
-
 azimuth = 70.54
 altitude = 37.876
-scale = 6.78
+scale = 9.78
 
 
-with rasterio.open('dsm.xxxx.tif') as dsmlayer:
-    dsmimg = dsmlayer.read(1)
+# when importing a DSM use something like this
+# with rasterio.open('dsm.xxxx.tif') as dsmlayer:
+#    dsmimg = dsmlayer.read(1)
+
+#creating an example dsm
+dsm = np.zeros((1000, 1000))
+dsm[300:400, 600:800] = 20
+dsm[700:750, 300:700] = 3
+dsm[100:300, 400:500] = 9
+
 
 
 with imageio.get_writer('animation/animation.gif', mode='I') as writer:
-    for x in range(0, 36):
-        sh = shadow_calc(dsmimg, azimuth + x, altitude - x/2, scale, resample="nearest")
+    for x in range(0, 50):
+        sh = shadow_calc(dsm, azimuth + x/2, altitude - x/4, scale, resample="bilinear")
         sh = sh*255
         writer.append_data(sh.astype(np.uint8))
 
